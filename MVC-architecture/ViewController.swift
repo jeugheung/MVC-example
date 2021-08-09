998691d51947d5124e99d1a5a43be1d37fdebf5c
Andrey Kim <akart.dev@gmail.com>
2021-08-10 00:58:21 +0600

    first commit

---
+++ MVC-architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-architecture
+//
+//  Created by Andrey Kim on 10.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
